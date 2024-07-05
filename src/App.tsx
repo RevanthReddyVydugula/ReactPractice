d69061e4a50b1049f322e0ba9a707dd3981b8f5f
RevanthReddyVydugula <revanth.vydugula@zemosolabs.com>
2024-07-05 10:41:22 +0530

    React app and Child components

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import './App.css';
+import Parent from './Parent';
+
+import React from 'react';
+
+const LifecycleDemo: React.FC = () => {
+  
+  return (
+    <div className='app'>
+      <Parent />
+    </div>
+  );
+};
+
+export default LifecycleDemo;
+
---
+++ src/Child.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+interface Props{
+    children: React.ReactNode;
+    onClick?: () => void; 
+};
+
+const Child:React.FC<Props> =(props) =>{
+    
+    return(
+        <button className='button' onClick={props.onClick}>{props.children}</button>
+    );
+}
+
+export default Child;
